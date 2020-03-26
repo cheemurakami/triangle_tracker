2605b0036bcb99bf561edf6b9a8189fe76bcb908
Chisato Murakami <kiwipom1031@gmail.com>
2020-03-26 15:13:45 -0700

    WE DID IT!! Add all functions.

--- js/scripts.js
+++ js/scripts.js
@@ -4,25 +4,35 @@ $(document).ready(function(){
     var side2 = parseInt($("input#side2").val());
     var side3 = parseInt($("input#side3").val());
     var answer = "";
+    
+    // if (side1 !== side2) {
+    //   if (side2 !== side3) {
+    //   }
+    //     if (side1 !==side3) {
+    //       answer = "scalene";  
+    //     }
+    // }
+    // if (side1 === side2 || side2 === side3) {
+    //    answer = "isosceles";
+    // }
+    // if (side1 === side2 && side1 === side3) {
+    //     answer = "equilateral";
+    // }
 
-    if (side1 === side2 && side1 === side3) {
-        answer = "equilateral"
-        return alert(answer);
-    }
-    if (side1 === side2 || side1 === side3 || side2 === side3) {
-      answer = "isosceles"
-      return alert(answer);
+    
+    if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1) {
+      answer = "not a "
+    } else if (side1 !== side2 && side2 !== side3 && side1 !== side3) {
+       answer = "scalene";
+    } else if (side1 === side2 && side1 === side3) {
+        answer = "equilateral";
+    } else {
+        answer = 'isosceles';
     }
 
-    if (side1 !== side2) {
-      if (side2 !== side3) {
-      }
-        answer = "scalene";
-        return alert(answer);
-    }
 
     $("#answer").empty().append(answer);
-      $(".answer-box").show();
+      $("#answer-box").show();
 
     event.preventDefault();
   });
